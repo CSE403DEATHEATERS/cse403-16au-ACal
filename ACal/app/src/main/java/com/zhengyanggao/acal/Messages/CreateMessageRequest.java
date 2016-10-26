8716dc1dd7fc17bb1d6203657ecc76fcc1158c61
Zhengyang Gao <gaoz6@cs.washington.edu>
2016-10-26 15:40:55 -0700

    Add more documentation to CreateMessageRequest.

--- ACal/app/src/main/java/com/zhengyanggao/acal/Messages/CreateMessageRequest.java
+++ ACal/app/src/main/java/com/zhengyanggao/acal/Messages/CreateMessageRequest.java
@@ -5,21 +5,48 @@ package com.zhengyanggao.acal.Messages;
  */
 public class CreateMessageRequest {
 
+    /**
+     * The category of the message
+     * */
     private MessageCategory messageCategory;
+
+    /**
+     * The id of the event with which the message is associated
+     * */
     private int eventId;
 
+    /**
+     * Return the message category of this CreateMessageRequest
+     *
+     * @return the message category of this CreateMessageRequest
+     * */
     public MessageCategory getMessageCategory() {
         return this.messageCategory;
     }
 
+    /**
+     * Set the message category of this CreateMessageRequest
+     *
+     * @param messageCategory
+     * */
     public void setMessageCategory(MessageCategory messageCategory) {
         this.messageCategory = messageCategory;
     }
 
+    /**
+     * Get the event id
+     *
+     * @return event id
+     * */
     public int getEventId() {
         return this.eventId;
     }
 
+    /**
+     * Set the event id
+     *
+     * @param eventId
+     * */
     public void setEventId(int eventId) {
         this.eventId = eventId;
     }
