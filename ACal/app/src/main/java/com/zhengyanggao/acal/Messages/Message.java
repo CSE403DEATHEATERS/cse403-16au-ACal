7134b58588d143fbbed1eb277d8e25aa63a828de
Zhengyang Gao <gaoz6@cs.washington.edu>
2016-10-26 15:51:37 -0700

    Add Message class.

---
+++ ACal/app/src/main/java/com/zhengyanggao/acal/Messages/Message.java
@@ -0,0 +1,88 @@
+package com.zhengyanggao.acal.Messages;
+
+import java.util.Date;
+import java.util.IllegalFormatCodePointException;
+
+/**
+ * Created by ZhengyangGao on 10/26/16.
+ */
+public class Message {
+
+    /**
+     * The id of the message
+     * */
+    private int messageId;
+
+    /**
+     * The category of the message
+     * */
+    private MessageCategory messageCategory;
+
+    /**
+     * The user that triggered the event that is creating this message
+     * */
+    private int createdBy;
+
+    /**
+     * Time when the Message is created
+     * */
+    private Date createdAt;
+
+    /**
+     * The event id with which this message is associated
+     * */
+    private int eventId;
+
+    /////////////////////////////////////////////////////////////////
+    /////                getters and setters                   /////
+    ////////////////////////////////////////////////////////////////
+
+    public int getMessageId() {
+        return this.messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public MessageCategory getMessageCategory() {
+        return this.messageCategory;
+    }
+
+    public void setMessageCategory(MessageCategory messageCategory) {
+        if (messageCategory == null) {
+            throw new IllegalArgumentException("messageCategory passed in should not be null.");
+        }
+
+        this.messageCategory = messageCategory;
+    }
+
+    public int getCreatedBy() {
+        return this.createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        if (createdAt == null) {
+            throw new IllegalArgumentException("createdAt passed in should not be null.");
+        }
+
+        this.createdAt = createdAt;
+    }
+
+    public int getEventId() {
+        return this.eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+}
