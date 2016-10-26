34db75e2a06a05308be33a3e95636fb9e8351879
Zhengyang Gao <gaoz6@cs.washington.edu>
2016-10-26 16:05:30 -0700

    Add method stubs for messages API's.

--- ACal/app/src/main/java/com/zhengyanggao/acal/Messages/MessageActivity.java
+++ ACal/app/src/main/java/com/zhengyanggao/acal/Messages/MessageActivity.java
@@ -5,10 +5,36 @@ package com.zhengyanggao.acal.Messages;
  */
 public class MessageActivity {
 
-    public void createMessage() {
-        
+    /**
+     * Create a message and store it in our database.
+     *
+     * @param request the request object used to create a message
+     * @throws IllegalArgumentException if request passed in is null
+     * @return a CreateMessageResponse containing the id of the message created
+     * */
+    public CreateMessageResponse createMessage(CreateMessageRequest request) {
+        CreateMessageResponse response = new CreateMessageResponse();
+
+
+
+
+        return response;
     }
 
+    /**
+     * Get a certain amount of messages from our database.
+     *
+     * @param request the request object used to create a message
+     * @throws IllegalArgumentException if request passed in is null
+     * @return a list of Message's that match the criteria described in the request
+     * */
+    public GetMessagesResponse getMessages(GetMessagesRequest request) {
+        GetMessagesResponse response = new GetMessagesResponse();
 
+
+
+
+        return response;
+    }
     
 }
