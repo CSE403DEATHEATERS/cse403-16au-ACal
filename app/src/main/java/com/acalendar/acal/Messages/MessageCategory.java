a7ba3c3d65f325c73871c18d3334840acdb5c8a0
rettymoo <rettymoo@hotmail.com>
2016-10-27 20:41:37 -0700

    done

---
+++ app/src/main/java/com/acalendar/acal/Messages/MessageCategory.java
@@ -0,0 +1,11 @@
+package com.acalendar.acal.Messages;
+
+/**
+ * Created by ZhengyangGao on 10/26/16.
+ */
+public enum MessageCategory {
+    NEW_TIME_PROPOSED,
+    TIME_CHANGED,
+    USER_JOINED_EVENT,
+    USER_QUIT_EVENT
+}
