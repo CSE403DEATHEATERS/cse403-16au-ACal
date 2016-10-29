3f91e108645d30dc66836dbecbd00a6d64204de8
shent3 <St123698*>
2016-10-28 20:58:26 -0700

    set up main ui

---
+++ app/src/main/java/com/acalendar/acal/Email/EmailManager.java
@@ -0,0 +1,12 @@
+package com.acalendar.acal.Email;
+
+/**
+ * Created by tongshen on 10/28/16.
+ */
+
+public class EmailManager {
+
+    public static boolean sendEmail() {
+        return false;
+    }
+}
