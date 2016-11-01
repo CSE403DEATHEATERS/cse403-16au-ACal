edb225e440f4801a572b2803ba539346870f552d
Zhengyang Gao <gaoz6@cs.washington.edu>
2016-11-01 14:44:19 -0700

    Trying to get my local branch to track master. Testing.

--- app/src/main/java/com/acalendar/acal/Messages/MessageActivity.java
+++ app/src/main/java/com/acalendar/acal/Messages/MessageActivity.java
@@ -2,6 +2,14 @@ package com.acalendar.acal.Messages;
 
 import android.app.Activity;
 
+/**
+ *
+ * Class that manages event messages.
+ *
+ * @author ZhengyangGao 
+ * 11/1/16
+ *
+ * */
 public class MessageActivity extends Activity{
 
     public void createMessage() {
