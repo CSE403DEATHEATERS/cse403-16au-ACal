3c2166e7ab240cb5bf2c5503182b1d8ae5f9ef17
Zhengyang Gao <samgaoofficial@gmail.com>
2016-11-06 17:40:32 -0800

    Implement getMessages()

--- server/src/main/java/lambda/MessageHandler.java
+++ server/src/main/java/lambda/MessageHandler.java
@@ -1,22 +1,24 @@
 package lambda;
 
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.datapipeline.model.Query;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 
 import model.*;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.math.BigDecimal;
+import java.util.*;
 
 public class MessageHandler {
 
@@ -52,8 +54,50 @@ public class MessageHandler {
     }
 
     public GetMessagesResponse getMessages(GetMessagesRequest request, Context context) {
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient().withEndpoint("https://dynamodb.us-west-2.amazonaws.com");
+        DynamoDB dynamoDB = new DynamoDB(client);
+        Table messagesTable = dynamoDB.getTable("acalendar-mobilehub-1275254137-messages");
+        Table messageAttributesTable = dynamoDB.getTable("acalendar-mobilehub-1275254137-message_attributes");
 
-        return null;
+        List<Message> messages = new ArrayList<Message>();
+        Index eventIdIndex = messagesTable.getIndex("eventIdIndex");
+        QuerySpec spec = new QuerySpec()
+                .withKeyConditionExpression("#s = :v_eventId")
+                .withNameMap(new NameMap()
+                        .with("#s", "eventId"))
+                .withValueMap(new ValueMap()
+                        .withString(":v_eventId", "an event id"));
+
+        ItemCollection<QueryOutcome> items = eventIdIndex.query(spec);
+        Iterator<Item> messagesTableIter = items.iterator();
+        while (messagesTableIter.hasNext()) {
+            Item messagesTableRow = messagesTableIter.next();
+            String messageId = (String) messagesTableRow.get("id");
+
+            // For each messageId, retrieve the corresponding records
+            // from message_attributes table.
+            PrimaryKey primaryKey = new PrimaryKey();
+            primaryKey.addComponent("messageId", "10");
+            primaryKey.addComponent("attributeType", MessageAttributeType.MESSAGE_CONTENT.toString());
+            GetItemSpec getItemSpec = new GetItemSpec().withPrimaryKey(primaryKey);
+            Item messageAttributesRow = messageAttributesTable.getItem(getItemSpec);
+
+            Message message = new Message();
+            message.setMessageId(messageId);
+            Date createdAt = new Date(((BigDecimal) messagesTableRow.get("createdAt")).longValue());
+            message.setCreatedAt(createdAt);
+            message.setCreatedBy((String) messagesTableRow.get("createdBy"));
+            message.setEventId((String) messagesTableRow.get("eventId"));
+            message.setMessageCategory(MessageCategory.valueOf((String) messagesTableRow.get("messageCategory")));
+            message.setMessageContent((String) messageAttributesRow.get("attributeValue"));
+
+            messages.add(message);
+        }
+
+        GetMessagesResponse response = new GetMessagesResponse();
+        response.setMessages(messages);
+
+        return response;
     }
 
     /**
