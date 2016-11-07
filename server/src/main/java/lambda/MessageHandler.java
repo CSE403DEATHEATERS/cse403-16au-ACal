602742e316316b0991969279afef8ab7292b3f8b
Zhengyang Gao <samgaoofficial@gmail.com>
2016-11-06 18:06:09 -0800

    Add comment and parameter validity checking.

--- server/src/main/java/lambda/MessageHandler.java
+++ server/src/main/java/lambda/MessageHandler.java
@@ -1,8 +1,6 @@
 package lambda;
 
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.datapipeline.model.Query;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
@@ -10,11 +8,7 @@ import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
 import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
 import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
-import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
-
 import model.*;
 
 import java.math.BigDecimal;
@@ -22,7 +16,16 @@ import java.util.*;
 
 public class MessageHandler {
 
+    /**
+     * Create a message and store it in our db
+     *
+     * @param request the request obejct used to construct the new record(s) in table(s)
+     * @param context an Android specific param
+     * @return a CreateMessageResponse containing the id of the record in the db
+     */
     public CreateMessageResponse createMessage(CreateMessageRequest request, Context context) {
+        validateCreateMessageRequest(request);
+
         AmazonDynamoDBClient client = new AmazonDynamoDBClient().withEndpoint("https://dynamodb.us-west-2.amazonaws.com");
         DynamoDB dynamoDB = new DynamoDB(client);
         Table messagesTable = dynamoDB.getTable("acalendar-mobilehub-1275254137-messages");
@@ -53,7 +56,17 @@ public class MessageHandler {
         return response;
     }
 
+    /**
+     * Retrieve messages associated with a specific event from db
+     *
+     * @param request an request object containing criteria used to retrieve records from db
+     * @param context an Android specific param
+     * @return a GetMessagesResponse containing a list of Messages matching the criteria
+     *         specified by the request.
+     */
     public GetMessagesResponse getMessages(GetMessagesRequest request, Context context) {
+        validateGetMessagesRequest(request);
+
         AmazonDynamoDBClient client = new AmazonDynamoDBClient().withEndpoint("https://dynamodb.us-west-2.amazonaws.com");
         DynamoDB dynamoDB = new DynamoDB(client);
         Table messagesTable = dynamoDB.getTable("acalendar-mobilehub-1275254137-messages");
@@ -98,6 +111,19 @@ public class MessageHandler {
         response.setMessages(messages);
 
         return response;
+    }
+
+    private void validateCreateMessageRequest(CreateMessageRequest request) {
+        if ((request == null) || (request.getEventId() == null) || (request.getMessageCategory() == null)
+                || (request.getMessageContent() == null) || (request.getUserId() == null)) {
+            throw new IllegalArgumentException("Request passed in and its fields should not be null!");
+        }
+    }
+
+    private void validateGetMessagesRequest(GetMessagesRequest request) {
+        if ((request == null) || (request.getEventId() == null)) {
+            throw new IllegalArgumentException("Request passed in and its fields should not be null!");
+        }
     }
 
     /**
