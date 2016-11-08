681498321570f9667530c082e0a04231faf98fa9
shent3 <St123698*>
2016-11-07 16:42:45 -0800

    email module if functioning correctly

--- server/src/main/java/model/Email.java
+++ server/src/main/java/model/Email.java
@@ -1,5 +1,6 @@
 package model;
 
+import javax.mail.Address;
 import java.util.List;
 import java.util.UUID;
 
@@ -10,18 +11,38 @@ public class Email {
     private String type;
     private String title;
     private String htmlContent;
-    private List<String> recipients;
-    private String sender;
+    private Address[] recipients;
+    private Address sender;
 
-    public Email(String type) {
+    public Email(String type, String title, String content, Address sender, Address[] recipients) {
         this.type = type;
+        this.sender = sender;
+        this.recipients = recipients;
+        this.buildTitle(title);
+        this.buildContent(content);
     }
 
-    private String buildTitle() {
-        return "";
+    private void buildTitle(String title) {
+        this.title = title;
     }
 
-    private String buildContent() {
-        return "";
+    private void buildContent(String content) {
+         this.htmlContent = content;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getHtmlContent() {
+        return this.htmlContent;
+    }
+
+    public Address getSender() {
+        return this.sender;
+    }
+
+    public Address[] getRecipients() {
+        return this.recipients;
     }
 }
