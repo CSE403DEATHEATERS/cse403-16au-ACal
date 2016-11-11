4176217f766fde2d17f3a2a5dd96c65f860be592
rettymoo <rettymoo@hotmail.com>
2016-11-10 18:18:48 -0800

    implementing event

--- server/src/main/java/model/Event.java
+++ server/src/main/java/model/Event.java
@@ -1,6 +1,11 @@
 package model;
 
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Event {
     private String eventId;
@@ -11,13 +16,53 @@ public class Event {
     private Date endTime;
     private String description;
     private Location location;
+    private List<Map<String, String>> attendee;
     private boolean isPublic;
+
+    public Map<String, AttributeValue> getInfo() {
+        Map<String, AttributeValue> info = new HashMap<String, AttributeValue>();
+        AttributeValue eventId = new AttributeValue();
+        eventId.setS(this.eventId);
+        AttributeValue ownerId = new AttributeValue();
+        ownerId.setS(this.ownerId);
+        AttributeValue title = new AttributeValue();
+        title.setS(this.title);
+        AttributeValue eventId = new AttributeValue();
+        eventId.setS(this.eventId);
+        AttributeValue eventId = new AttributeValue();
+        eventId.setS(this.eventId);
+        AttributeValue eventId = new AttributeValue();
+        eventId.setS(this.eventId);
+        AttributeValue eventId = new AttributeValue();
+        eventId.setS(this.eventId);
+        info.put("eventId", );
+
+        return info;
+    }
 
     public class Location {
         double lat;
         double lng;
+        String address;
         int postal;
+        String state;
+        String streetName;
+        String streetNumber;
 
+        public Location(double lat, double lng, String address, int postal, String state, String streetName, String streetNumber) {
+            this.lat =  lat;
+            this.lng = lng;
+            this.address = address;
+            this.postal = postal;
+            this.state = state;
+            this.streetName = streetName;
+            this.streetNumber = streetNumber;
+        }
+
+        public Map<String, AttributeValue> getInfo() {
+            Map<String, AttributeValue> info = new HashMap<String, AttributeValue>();
+
+        }
     }
 
 }
