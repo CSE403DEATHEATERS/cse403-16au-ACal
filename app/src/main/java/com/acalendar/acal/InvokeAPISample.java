7a9a4c2ea6caa926853f80dd782a5dd9ca956233
shent3 <St123698*>
2016-11-11 15:02:05 -0800

    fixed could not found gson problem. ready for hookup

--- app/src/main/java/com/acalendar/acal/InvokeAPISample.java
+++ app/src/main/java/com/acalendar/acal/InvokeAPISample.java
@@ -4,6 +4,7 @@ import android.net.UrlQuerySanitizer;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 
+import com.acalendar.acal.Login.LoginActivity;
 import com.acalendar.acal.amazonaws.mobile.AWSMobileClient;
 import com.acalendar.acal.amazonaws.mobile.api.CloudLogicAPI;
 import com.acalendar.acal.amazonaws.mobile.api.CloudLogicAPIConfiguration;
@@ -21,25 +22,22 @@ import java.util.List;
 import java.util.Map;
 
 public class InvokeAPISample {
+    public static String apiResponse;
+
     public static void main(String[] args) {
-        invokeAPI("POST", "/login", "");
     }
 
-    public static void invokeAPI(String method, String path, String body) {
-        // Set your request method, path, query string parameters, and request body
-//        method = "POST";
-//        path = "/login";
-//        body = "{\"myfriend\":\"hehe\"}";
-        Map<String, String> queryStringParameters = new HashMap<String, String>();
+    public static String invokeAPI(String method, String path, String body, String query) {
+        Map<String, String> queryStringParameters = convertQueryStringToParameters(query);
         Map<String, String> headers = new HashMap<String, String>();
-        CloudLogicAPIConfiguration[] apiConfigurations = CloudLogicAPIFactory.getAPIs();
+        final CloudLogicAPIConfiguration[] apiConfigurations = CloudLogicAPIFactory.getAPIs();
+
+        final byte[] content = body.getBytes(StringUtils.UTF8);
 
         final CloudLogicAPI client =
                 AWSMobileClient.defaultMobileClient().createAPIClient(apiConfigurations[0].getClientClass());
 
-        final byte[] content = body.getBytes(StringUtils.UTF8);
-
-        ApiRequest tmpRequest =
+        final ApiRequest tmpRequest =
                 new ApiRequest(client.getClass().getSimpleName())
                         .withPath(path)
                         .withHttpMethod(HttpMethodName.valueOf(method))
@@ -57,7 +55,6 @@ public class InvokeAPISample {
         } else {
             request = tmpRequest;
         }
-
         // Make network call on background thread
         new Thread(new Runnable() {
             Exception exception = null;
@@ -67,17 +64,37 @@ public class InvokeAPISample {
                 try {
 //                    Log.d(LOG_TAG, "Invoking API w/ Request : " + request.getHttpMethod() + ":" + request.getPath());
 
-                    long startTime = System.currentTimeMillis();
-
                     final ApiResponse response = client.execute(request);
 
                     final String responseData = IOUtils.toString(response.getContent());
+                    apiResponse = responseData;
 
                 } catch (final Exception exception) {
                     exception.printStackTrace();
                 }
             }
         }).start();
+        return apiResponse;
+    }
 
+    private static Map<String,String> convertQueryStringToParameters(String queryStringText) {
+
+        while (queryStringText.startsWith("?") && queryStringText.length() > 1) {
+            queryStringText = queryStringText.substring(1);
+        }
+
+        final UrlQuerySanitizer sanitizer = new UrlQuerySanitizer();
+        sanitizer.setAllowUnregisteredParamaters(true);
+        sanitizer.parseQuery(queryStringText);
+
+        final List<UrlQuerySanitizer.ParameterValuePair> pairList = sanitizer.getParameterList();
+        final Map<String, String> parameters = new HashMap<>();
+
+        for (final UrlQuerySanitizer.ParameterValuePair pair : pairList) {
+            Log.d("LOG", pair.mParameter + " = " + pair.mValue);
+            parameters.put(pair.mParameter, pair.mValue);
+        }
+
+        return parameters;
     }
 }
--- app/src/main/java/com/acalendar/acal/Login/LoginActivity.java
+++ app/src/main/java/com/acalendar/acal/Login/LoginActivity.java
@@ -9,20 +9,22 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.acalendar.acal.InvokeAPISample;
-import com.acalendar.acal.LambdaInvoker.LambdaInterface;
-import com.acalendar.acal.LambdaInvoker.NameInfo;
 import com.acalendar.acal.MainActivity;
 import com.acalendar.acal.R;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSCredentialsProviderChain;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.http.JsonErrorResponseHandler;
 import com.amazonaws.mobileconnectors.lambdainvoker.LambdaFunctionException;
 import com.amazonaws.mobileconnectors.lambdainvoker.LambdaInvokerFactory;
 import com.amazonaws.regions.Regions;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.HashMap;
 
 
 public class LoginActivity extends Activity {
-    public static final Regions REGION = Regions.US_WEST_2;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -62,22 +64,19 @@ public class LoginActivity extends Activity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                InvokeAPISample.invokeAPI("GET", "/login", "{}");
+                String apiResponse = InvokeAPISample.invokeAPI("GET", "/login", "", "?username=myfriend&password=hehe");
+                String fullname = getFullname(apiResponse);
                 finish();
             }
         });
     }
 
-    private LambdaInvokerFactory invokeLogin() {
-        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
-                this.getApplicationContext(),
-                "us-west-2_UU8IM4FSD",
-                REGION);
-        LambdaInvokerFactory factory = new LambdaInvokerFactory(
-                this.getApplicationContext(),
-                REGION,
-                credentialsProvider);
-        return factory;
+    private String getFullname(String json) {
+        HashMap<String,String> map = new Gson().fromJson(json, new TypeToken<HashMap<String, String>>(){}.getType());
+        if (map.containsKey("firstname") || map.containsKey("lastname"))
+            return "" + map.get("firsname") + map.get("lastname");
+        else
+            return "";
     }
 
 }
