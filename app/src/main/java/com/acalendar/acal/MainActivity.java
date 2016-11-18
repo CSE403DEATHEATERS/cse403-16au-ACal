2368a4d95a70b496e8e40087cd41d17ffe23fdd3
Zhengyang Gao <samgaoofficial@gmail.com>
2016-11-18 14:45:37 -0800

    Resolving merge issue.

--- app/src/main/java/com/acalendar/acal/MainActivity.java
+++ app/src/main/java/com/acalendar/acal/MainActivity.java
@@ -100,6 +100,8 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+
+    
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
