bc50c20557e0c542c97f83846e056d40e94cf5e0
ziyaoh <ziyaoh@uw.edu>
2016-11-18 02:39:38 -0800

    initializing unittest for event sharing functions

---
+++ server/src/test/java/EventSharingHandlerTest.java
@@ -0,0 +1,21 @@
+import org.hibernate.cfg.NotYetImplementedException;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by Yaozi on 11/18/16.
+ */
+public class EventSharingHandlerTest {
+
+    @Ignore("not ready yet")
+    @Test
+    public void TestEditEventAttendees() {
+        throw new NotYetImplementedException();
+    }
+
+    @Ignore("not ready yet")
+    @Test
+    public void testHandleEventInvitation() {
+        throw new NotYetImplementedException();
+    }
+}
---
+++ server/src/test/java/EventSharingServiceTest.java
@@ -0,0 +1,33 @@
+import org.hibernate.cfg.NotYetImplementedException;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by Yaozi on 11/18/16.
+ */
+public class EventSharingServiceTest {
+
+    @Ignore("not ready yet")
+    @Test
+    public void TestEditAttendees() {
+        throw new NotYetImplementedException();
+    }
+
+    @Ignore("not ready yet")
+    @Test
+    public void testAcceptEventInvitation() {
+        throw new NotYetImplementedException();
+    }
+
+    @Ignore("not ready yet")
+    @Test
+    public void testDeclineEventInvitation() {
+        throw new NotYetImplementedException();
+    }
+
+    @Ignore("not ready yet")
+    @Test
+    public void testGetPendingEventsByUserId() {
+        throw new NotYetImplementedException();
+    }
+}
