280fe6990f85b03b764e4e898068a6c8d9c413e7
QINRONG LIN <snailin@cs.washington.edu>
2016-11-21 13:39:36 -0800

    UI

--- app/src/main/java/com/acalendar/acal/NotifiFragment.java
+++ app/src/main/java/com/acalendar/acal/NotifiFragment.java
@@ -6,6 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.acalendar.acal.Notify.EventsNotifiAdapter;
+import com.acalendar.acal.Notify.FriendsNotifiAdapter;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,6 +20,7 @@ import android.view.ViewGroup;
  */
 public class NotifiFragment extends Fragment {
 
+    private View view;
 
     public NotifiFragment() {
         // Required empty public constructor
@@ -23,7 +31,52 @@ public class NotifiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifi, container, false);
+        view =  inflater.inflate(R.layout.fragment_notifi, container, false);
+
+        notiEventsView();
+        notiFriendsView();
+        return view;
+    }
+
+    private void notiEventsView() {
+
+        ArrayList<String> notifyEventsList = new ArrayList<>();
+        notifyEventsList.add("Momo invite you to event1");
+        notifyEventsList.add("Momo invite you to event2");
+        notifyEventsList.add("Lisa invite you to event1");
+
+        EventsNotifiAdapter adapter = new EventsNotifiAdapter(getActivity(), R.layout.notifi_events_item ,R.id.noti_events_item, notifyEventsList);
+
+
+        ListView listView = (ListView) view.findViewById(R.id.noti_events_listview);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+            }
+        });
+
+    }
+
+    private void notiFriendsView() {
+        ArrayList<String> notifyEventsList = new ArrayList<>();
+        notifyEventsList.add("Momo wants to be you friend");
+        notifyEventsList.add("Lisa wants to be you friend");
+
+        FriendsNotifiAdapter adapter = new FriendsNotifiAdapter(getActivity(), R.layout.notifi_friends_item ,R.id.noti_friend_item, notifyEventsList);
+
+
+        ListView listView = (ListView) view.findViewById(R.id.noti_friends_listview);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+            }
+        });
     }
 
 }
