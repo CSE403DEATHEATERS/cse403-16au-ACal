95014cfd4d5b380d2bc9d8908ae5d6d6cd501765
zq0605 <zq0605@uw.edu>
2016-11-21 18:09:46 -0800

    friend manager with add and accept finish

--- app/src/main/java/com/acalendar/acal/Friend/FriendManager.java
+++ app/src/main/java/com/acalendar/acal/Friend/FriendManager.java
@@ -1,6 +1,19 @@
 package com.acalendar.acal.Friend;
 
+import android.util.Log;
+
+import com.acalendar.acal.ApiResource;
+import com.acalendar.acal.Login.LoginedAccount;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class manages the friends features including adding friends,
@@ -9,25 +22,79 @@ import java.util.List;
  */
 
 public class FriendManager {
-    private List<Friend> listOfFriend;
+    private static List<Friend> listOfFriend;
 
     public FriendManager() {
-
+        listOfFriend = new ArrayList<>();
+        // call get all friends list, parse responce map
+        Map<String, String> query = new HashMap<>();
+        query.put("userId", LoginedAccount.getUserId());
+        Map<String, Object> apiResponse = ApiResource.submitRequest(
+                query, null,
+                ApiResource.GET_REQUEST,
+                ApiResource.REQUEST_GET_FRIENDS);
+        List<Map<String, String>> friendsResponse =
+                (List) apiResponse.get("ACCEPT");
+        if (!friendsResponse.isEmpty()) {
+            for (Map<String, String> friend : friendsResponse) {
+                Friend thisFriend = new Friend(
+                        friend.get("lastname"),
+                        friend.get("firstname"),
+                        friend.get("email"),
+                        friend.get("username"),
+                        friend.get("userId"));
+                listOfFriend.add(thisFriend);
+            }
+        }
+        // accept. pending(to be accepted), sent(to others)
     }
 
     public List<Friend> getListOfFriend() {
-        return null;
+        return Collections.unmodifiableList(listOfFriend);
     }
 
-    public boolean addFriend() {
+    public boolean addFriend(String userInput) {
+        Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+        Matcher m = p.matcher(userInput);
+        Map<String, String> bodyMap = new HashMap<>();
+        bodyMap.put("userId_1", LoginedAccount.getUserId());
+        if (m.matches()) {
+            bodyMap.put("email", userInput);
+        } else {
+            bodyMap.put("username", userInput);
+        }
+        JSONObject jsonBody = new JSONObject(bodyMap);
+        String body = jsonBody.toString();
+        Map<String, Object> apiResponse = ApiResource.submitRequest(
+                new HashMap<String, String>(), body,
+                ApiResource.POST_REQUEST, ApiResource.REQUEST_ADD_FRIEND);
+        if (apiResponse.get("result") != null) {
+            if (apiResponse.get("result").equals("true")) {
+                //TODO: give feedback message
+            } else {
+                //TODO: ask for input again
+            }
+        }
         return false;
+    }
+
+    public boolean acceptFriend(Friend f) {
+        // uid_1, uid_2, result, true, post
+        Map<String, String> addFriendQuery = new HashMap<>();
+        addFriendQuery.put("userId_1", LoginedAccount.getUserId());
+        addFriendQuery.put("userId_2", f.getUserId());
+        Map<String, Object> apiResponse = ApiResource.submitRequest(
+                addFriendQuery, null,
+                ApiResource.GET_REQUEST,
+        ApiResource.REQUEST_ACEEPT_FRIEND);
+        if (apiResponse.get("result") == null) {
+            return false;
+        }
+        Log.v("FriendManager", "accepted friend request from " + f.getName());
+        return true;
     }
 
     public boolean deleteFriend() {
-        return false;
-    }
-
-    public boolean acceptFriend() {
         return false;
     }
 
