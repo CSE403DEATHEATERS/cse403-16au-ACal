5ee144d09ded449a6b891fe659e444cb21de05c4
shent3 <St123698*>
2016-12-02 19:53:11 -0800

    added public eventt pool

--- app/src/main/java/com/acalendar/acal/PublicEventsPoolFragment.java
+++ app/src/main/java/com/acalendar/acal/PublicEventsPoolFragment.java
@@ -7,20 +7,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
+import com.acalendar.acal.Login.LoginedAccount;
+import com.acalendar.acal.Notification.Invitation;
+import com.acalendar.acal.Notification.InvitationAdapter;
+import com.google.android.gms.common.api.Api;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PublicEventsPoolFragment extends Fragment implements OnMapReadyCallback {
-    private GoogleMap mMap;
+public class PublicEventsPoolFragment extends Fragment {
+//    private GoogleMap mMap;
+    InvitationAdapter invitationAdapter;
 
 
     public PublicEventsPoolFragment() {
@@ -41,26 +56,83 @@ public class PublicEventsPoolFragment extends Fragment implements OnMapReadyCall
         getActivity().setTitle("Public Events Pool");
 
         Spinner eventTypeFilterDropdown = (Spinner) view.findViewById(R.id.publicEventsPoolEventTypeFilterSpinner);
-        String[] items = new String[]{"Party", "Convert"};
+        String[] items = new String[]{"Party", "Concert"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items);
         eventTypeFilterDropdown.setAdapter(adapter);
+//
+//        Spinner startTimeFilterDropdown = (Spinner)view.findViewById(R.id. publicEventsPoolCitySpinner);
+//        String[] items2 = new String[]{"Seattle", "Bellevue", "Lake City"};
+//        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items2);
+//        startTimeFilterDropdown.setAdapter(adapter2);
 
-        Spinner startTimeFilterDropdown = (Spinner)view.findViewById(R.id. publicEventsPoolCitySpinner);
-        String[] items2 = new String[]{"Seattle", "Bellevue", "Lake City"};
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, items2);
-        startTimeFilterDropdown.setAdapter(adapter2);
+
+        ArrayList<Invitation> invitationsList = new ArrayList<>();
+
+        // query to get all public events
+        Map<String, String> query = new HashMap<String, String>();
+        Map<String, Object> response = ApiResource.submitRequest(query, null, ApiResource.GET_REQUEST, ApiResource.REQUEST_PUBLIC_EVENT_POOL);
+        if (response.containsKey("result")) {
+            List<Map<String, Object>> publicEvents = (List) response.get("result");
+            invitationsList = parseInvitation(publicEvents);
+        }
+
+
+        invitationAdapter = new InvitationAdapter(getActivity(), R.layout.invitation_item, R.id.invitation, invitationsList);
+
+        ListView listView = (ListView) view.findViewById(R.id.event_pool_list_view);
+
+        listView.setAdapter(invitationAdapter);
 
         return view;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney, Australia, and move the camera.
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//    }
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private ArrayList<Invitation> parseInvitation(List<Map<String, Object>> mapList) {
+        ArrayList<Invitation> invitationList = new ArrayList<>();
+        for (Map<String, Object> eventMap : mapList) {
+            if (eventMap.isEmpty()) {
+                continue;
+            }
+            String title = "";
+            if (eventMap.get("title") != null) {
+                title = eventMap.get("title").toString();
+            }
+            String eventId = "";
+            if (eventMap.get("eventId") != null) {
+                eventId = eventMap.get("eventId").toString();
+            }
+            String startTime = "";
+            if (eventMap.get("startTime") != null) {
+                String start = eventMap.get("startTime").toString();
+                double startDouble = Double.parseDouble(start);
+                Long startLong = (long)startDouble;
+                Date startDate = new Date(startLong);
+                Format format = new SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.US);
+                startTime = format.format(startDate);
+            }
+//            String ownerName = "";
+//            if (eventMap.get("ownerName") != null) {
+//                ownerName = eventMap.get("ownerName").toString();
+////                ownerName = "Ruoyu Mo";
+//            }
+            String location = "";
+            if (eventMap.get("location") != null) {
+                Map<String, String> locationMap = (Map<String, String>)eventMap.get("location");
+                location = locationMap.get("address");
+            }
+            Invitation invitation = new Invitation(title, "", startTime, location, eventId, false);
+            invitationList.add(invitation);
+        }
+        return invitationList;
     }
 
 }
